5f1fc6076174a14b7afef914b8edc97f8fba7955
Bnula <bnulagnula@gmail.com>
2024-08-22 11:30:16 +0200

    initial commit

---
+++ src/app/ui/site-logo.tsx
@@ -0,0 +1,13 @@
+import { BookOpenIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function SiteLogo() {
+    return (
+        <div
+        className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+        >
+        <BookOpenIcon className="h-12 w-12 rotate-[15deg]" />
+        <p className="text-[44px]">Maki</p>
+        </div>
+    );
+}
